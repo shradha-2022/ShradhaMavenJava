ac8e559766f89e9a7cf0959c656dea29450255e1
shradha <shradhacg@gmail.com>
2023-11-10 20:27:03 +0530

    ShradhaMavenJavaproject

---
+++ src/main/java/SEGroup/ShradhaMavenJava/App.java
@@ -0,0 +1,14 @@
+package SEGroup.ShradhaMavenJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Shradha here,,,,,," );
+    }
+}
+
